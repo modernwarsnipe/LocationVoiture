dada98b255016ba77fa8b47afb800c04974c46e4
ADB <40320197+modernwarsnipe@users.noreply.github.com>
2021-01-13 22:14:33 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,34 @@
+'use strict';
+
+const PORT = 3080;
+const MONGO_URI = 'mongodb://localhost:27017';
+const DB_NAME = 'accounts';
+
+const bodyParser = require('body-parser');
+const express = require('express');
+const chalk = require('chalk');
+
+
+var app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+var apiRoutesAccounts = require('./routes/api');
+var htmRoutes = require('./routes/pug');
+
+var config = {
+    dbUri: MONGO_URI,
+    dbName: DB_NAME,
+    apiRoot: `http://localhost:${PORT}`
+}
+
+// Static views
+app.use(express.static('assets'));
+
+// Routes for the apiS
+apiRoutesAccounts.addRoutes(app, config);
+htmRoutes.addRoutes(app, config);
+
+app.listen(PORT, () => {
+    console.log(chalk.green.bold('Listening'), `to port ${PORT}`);
+});
